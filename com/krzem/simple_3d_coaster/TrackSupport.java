424f44b1b1b186e7d4eec5103470f02619fa3257
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 16:44:49 +0100

    Push Update

---
+++ com/krzem/simple_3d_coaster/TrackSupport.java
@@ -0,0 +1,30 @@
+package com.krzem.simple_3d_coaster;
+
+
+
+import com.jogamp.opengl.GL2;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class TrackSupport extends Constants{
+	public Main.Main_ cls;
+	public Game game;
+	public TrackSupportList tsl;
+	public Vector3[] bm;
+	public Vector3 ga;
+	public Vector3 gb;
+
+
+
+	public void update(GL2 gl){
+
+	}
+
+
+
+	public void draw(GL2 gl){
+
+	}
+}
