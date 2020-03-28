4e0a4d7d42249686544ef3121ee1e73dd683ff0f
Krzem <50489938+Krzem5@users.noreply.github.com>
2020-03-28 16:44:23 +0100

    Push Update

---
+++ com/krzem/simple_3d_coaster/Light.java
@@ -0,0 +1,17 @@
+package com.krzem.simple_3d_coaster;
+
+
+
+import com.jogamp.opengl.GL2;
+
+
+
+public class Light extends Constants{
+	public Vector3 pos;
+
+
+
+	public Light(Vector3 pos){
+		this.pos=pos;
+	}
+}
